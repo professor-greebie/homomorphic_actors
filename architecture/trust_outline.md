f13d83ca005cb9aa363535ed13edf1e09cade31d
Ryan Deschamps <rdeschamps@conestogac.on.ca>
2025-06-18 09:58:40 -0400

    Conform to linting strategy

---
+++ architecture/trust_outline.md
@@ -0,0 +1,10 @@
+# Actor-Based Homomorphic Trust
+
+```mermaid
+
+flowchart LR
+    start[Guardian] --> t1[Trust Actor]
+
+
+
+```
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use sids::actors::messages::Message;
 use sids::actors::actor::Actor;
 use sids::actors::messages::ResponseMessage;
 use sids::actors::{get_response_channel, send_message_by_id, spawn_actor, start_actor_system};
-use sids::actors::actor_system::ActorSystem;
 
 fn get_loggings() {
     let env = Env::default().filter_or("MY_LOG_LEVEL", "info");
@@ -27,12 +26,20 @@ struct TrustActor;
 impl Actor<TrustMessage> for TrustActor {
     async fn receive(&mut self, message: Message<TrustMessage>) where Self:Sized + 'static {
         if let Message {
-            payload: Some(TrustMessage::TrustAddKey { key }),
+            payload,
             stop: _,
             responder: Some(courrier), 
             blocking: _,
         } = message {
-            info!("Adding trust key: {}", key);
+
+            match payload.expect("Message received without payload.") {
+                TrustMessage::TrustAddKey { key } => {
+                    info!("Trust key added: {}", key);
+                }
+                TrustMessage::TrustRemoveKey { key } => {
+                    info!("Trust key removed: {}", key);
+                }
+            }
             let _ = courrier
                 .send(ResponseMessage::Success);
         }
@@ -44,8 +51,9 @@ impl Actor<TrustMessage> for TrustActor {
  async fn main() {
     get_loggings();
     let trust_actor = TrustActor{};
-    let mut actor_system = sids::actors::start_actor_system::<TrustMessage>();
-    let (tx, rx) = sids::actors::get_response_channel(&mut actor_system);
+    let mut actor_system = start_actor_system::<TrustMessage>();
+    let (tx, rx) = get_response_channel(&mut actor_system);
+    let (rtx,rrx) = get_response_channel(&mut actor_system);
     let message = Message {
         payload: Some(TrustMessage::TrustAddKey {
             key: "example_key".to_string(),
@@ -54,9 +62,21 @@ impl Actor<TrustMessage> for TrustActor {
         responder: Some(tx),
         blocking: None,
     };
+    let rem_message = Message {
+        payload: Some(TrustMessage::TrustRemoveKey {
+            key: "example_key".to_string(),
+        }),
+        stop: false,
+        responder: Some(rtx),
+        blocking: None,
+    };
     spawn_actor(&mut actor_system, trust_actor, Some("First Trust Actor".to_string())).await;
     send_message_by_id(&mut actor_system, 1, message).await;
     if let Ok(response) = rx.await {
+        info!("Received response: {:?}", response);
+    }
+    send_message_by_id(&mut actor_system, 1, rem_message).await;
+    if let Ok(response) = rrx.await {
         info!("Received response: {:?}", response);
     }
 }
